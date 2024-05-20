2a0b09da24cb31b6b8d3c7c9fc89323192084d1e
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2024-05-20 19:32:40 +0200

    add fourier methods and models

---
+++ lib/fourier.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:flutter_fourier_series/models.dart';
+
+class Fourier {
+  static List<DFTResultModel> discreteFourierTransform(
+    List<ComplexNumber> input,
+  ) {
+    final length = input.length;
+    final results = <DFTResultModel>[];
+
+    for (var frequency = 0; frequency < length; frequency++) {
+      var sum = const ComplexNumber(real: 0, imaginary: 0);
+
+      for (var i = 0; i < length; i++) {
+        final phi = (pi * 2 * frequency * i) / length;
+        final exp = ComplexNumber(real: cos(phi), imaginary: -sin(phi));
+
+        sum += input[i] * exp;
+      }
+
+      sum = sum.scale(1 / length);
+
+      final amplitude =
+          sqrt(sum.real * sum.real + sum.imaginary * sum.imaginary);
+      final phase = atan2(sum.imaginary, sum.real);
+
+      results.add(
+        DFTResultModel(
+          complex: sum,
+          frequency: frequency,
+          amplitude: amplitude,
+          phase: phase,
+        ),
+      );
+    }
+
+    return results;
+  }
+}
---
+++ lib/models.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ComplexNumber {
+  const ComplexNumber({required this.real, required this.imaginary});
+
+  final double real;
+  final double imaginary;
+
+  ComplexNumber operator +(ComplexNumber other) => ComplexNumber(
+        real: real + other.real,
+        imaginary: imaginary + other.imaginary,
+      );
+
+  ComplexNumber operator *(ComplexNumber other) => ComplexNumber(
+        real: real * other.real - imaginary * other.imaginary,
+        imaginary: real * other.imaginary + imaginary * other.real,
+      );
+
+  ComplexNumber scale(double scalar) => ComplexNumber(
+        real: real * scalar,
+        imaginary: imaginary * scalar,
+      );
+
+  Offset toOffset() => Offset(real, imaginary);
+}
+
+class DFTResultModel {
+  const DFTResultModel({
+    required this.complex,
+    required this.frequency,
+    required this.amplitude,
+    required this.phase,
+  });
+
+  final ComplexNumber complex;
+  final int frequency;
+  final double amplitude;
+  final double phase;
+}
